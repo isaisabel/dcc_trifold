eee44a9d6962dadde00dd0f8ade958e5a47a0dbd
Isabel Trahan <isa.isabel.26a7@gmail.com>
2019-10-23 17:16:12 -0400

    add new classes, updates to output format

--- trifold/make.sh
+++ trifold/make.sh
@@ -3,20 +3,26 @@ latexmk -quiet -pdf trifold.tex
 latexmk -c
 rm -rf output
 mkdir output
-pdftk A=trifold.pdf cat 2-3 output output/wizard-noflip.pdf
-pdftk A=trifold.pdf cat 5-6 output output/cleric-noflip.pdf
-pdftk A=trifold.pdf cat 8-9 output output/warrior-noflip.pdf
-pdftk A=trifold.pdf cat 11-12 output output/thief-noflip.pdf
-pdftk A=trifold.pdf cat 14-15 output output/dwarf-noflip.pdf
-pdftk A=trifold.pdf cat 17-18 output output/elf-noflip.pdf
-pdftk A=trifold.pdf cat 20-21 output output/halfling-noflip.pdf
+pdftk A=trifold.pdf cat 2-3 output output/wizard.pdf
+pdftk A=trifold.pdf cat 5-6 output output/cleric.pdf
+pdftk A=trifold.pdf cat 8-9 output output/warrior.pdf
+pdftk A=trifold.pdf cat 11-12 output output/thief.pdf
+pdftk A=trifold.pdf cat 14-15 output output/dwarf.pdf
+pdftk A=trifold.pdf cat 17-18 output output/elf.pdf
+pdftk A=trifold.pdf cat 20-21 output output/halfling.pdf
+pdftk A=trifold.pdf cat 23-24 output output/druid.pdf
+pdftk A=trifold.pdf cat 26-27 output output/witch.pdf
 
-pdftk A=output/wizard-noflip.pdf rotate 2south output output/wizard.pdf
-pdftk A=output/cleric-noflip.pdf rotate 2south output output/cleric.pdf
-pdftk A=output/warrior-noflip.pdf rotate 2south output output/warrior.pdf
-pdftk A=output/thief-noflip.pdf rotate 2south output output/thief.pdf
-pdftk A=output/dwarf-noflip.pdf rotate 2south output output/dwarf.pdf
-pdftk A=output/elf-noflip.pdf rotate 2south output output/elf.pdf
-pdftk A=output/halfling-noflip.pdf rotate 2south output output/halfling.pdf
+pdftk A=output/wizard.pdf rotate 2south output output/wizard-printable.pdf
+pdftk A=output/cleric.pdf rotate 2south output output/cleric-printable.pdf
+pdftk A=output/warrior.pdf rotate 2south output output/warrior-printable.pdf
+pdftk A=output/thief.pdf rotate 2south output output/thief-printable.pdf
+pdftk A=output/dwarf.pdf rotate 2south output output/dwarf-printable.pdf
+pdftk A=output/elf.pdf rotate 2south output output/elf-printable.pdf
+pdftk A=output/halfling.pdf rotate 2south output output/halfling-printable.pdf
+pdftk A=output/druid.pdf rotate 2south output output/druid-printable.pdf
+pdftk A=output/witch.pdf rotate 2south output output/witch-printable.pdf
+
+pdftk output/*-printable.pdf cat output output/all-printable.pdf
 
 rm trifold.pdf
